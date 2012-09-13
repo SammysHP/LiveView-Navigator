4cc3a5bebc13b8ed31b166ec1c806bb01b6c5ce6
keith.paterson <keith.paterson@gmx.de>
2012-09-13 07:08:13 +0200

    Save Destination to Prefs

--- src/org/cgeo/liveview/UpdateReceiver.java
+++ src/org/cgeo/liveview/UpdateReceiver.java
@@ -3,10 +3,14 @@ package org.cgeo.liveview;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.util.Log;
 import cgeo.geocaching.geopoint.Geopoint;
 import cgeo.geocaching.geopoint.GeopointFormatter;
 import cgeo.geocaching.geopoint.GeopointFormatter.Format;
+
+import com.sonyericsson.extras.liveview.plugins.PluginConstants;
 
 /**
  * This receiver updates the cached destination.<br />
@@ -20,17 +24,21 @@ import cgeo.geocaching.geopoint.GeopointFormatter.Format;
  */
 public class UpdateReceiver extends BroadcastReceiver {
 
-    @Override
+	@Override
     public void onReceive(Context context, Intent intent) {
 
 		try {
 			Float latitude = (Float) intent.getExtras().get("latitude");
 			Float longitude = (Float) intent.getExtras().get("longitude");
-			Log.v("LiveView", new GeopointFormatter().format(Format.LAT_LON_DECMINUTE_RAW, new Geopoint(latitude, longitude)));
-
+			SharedPreferences prefs = context.getSharedPreferences("org.cgeo.liveview_preferences", Context.MODE_WORLD_WRITEABLE);
+			Editor e = prefs.edit();
+			e.putFloat("latitude", latitude);
+			e.putFloat("longitude", longitude);
+			e.commit();
+			Log.v("LiveView received new Coords ", GeopointFormatter.format(Format.LAT_LON_DECMINUTE_RAW, new Geopoint(latitude, longitude)));
+			// context.getSharedPreferences(name, mode);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Log.e(PluginConstants.LOG_TAG, "Error in UpdateReceiver", e);
 		}
     }
 
